40d2af6c0ec73021005ba5c4aeb3a32bd56fa9b5
SamK <samuelkyama102@gmail.com>
2023-09-01 18:03:13 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<script>
+export default {
+  data() {
+    return {
+      myTitle: "My todo list",
+      todoItems: [
+        { name: "wash", description: "Wash dishes at 2pm" },
+        { name: "wash", description: "Wash car at 3pm" },
+        { name: "read", description: "Read for an exam" },
+      ],
+      name: "",
+      newTodoItem: {
+        name: "",
+        description: "",
+      },
+    };
+  },
+  methods: {
+    addTodo() {
+      this.todoItems.push(this.newTodoItem);
+    },
+  },
+};
+</script>
+
+<template>
+  <p>{{ myTitle }}</p>
+
+  {{ newTodoItem }}
+  <ul>
+    <li class="capitalize" v-for="(item, index) in todoItems" :key="index">
+      {{ index + 1 }}. {{ item.name }} {{ item.description }}
+    </li>
+  </ul>
+
+  <input v-model="newTodoItem.name" />
+  <input v-model="newTodoItem.description" />
+  <button v-on:click="addTodo">Add Todo</button>
+</template>
+
+<style scoped>
+.capitalize {
+  text-transform: capitalize;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<template>
+  <p class="capitalize">home</p>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
